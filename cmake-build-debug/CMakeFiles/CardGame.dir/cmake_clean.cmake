40065577ecfafd6adc062212194447bcc5a5a9e2
Akishi Kuwayama <patrick233@qq.com>
2022-03-09 14:44:33 +0800

    Initial

---
+++ cmake-build-debug/CMakeFiles/CardGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CardGame.dir/main.cpp.obj"
+  "CardGame.exe"
+  "CardGame.exe.manifest"
+  "CardGame.pdb"
+  "libCardGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CardGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
